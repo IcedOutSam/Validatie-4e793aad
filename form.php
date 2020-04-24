f987c43033e707a70787c657a251b915695cd3ca
Sam <2102926@talnet.nl>
2020-04-24 13:00:33 +0200

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -7,7 +7,9 @@
   <body>
     <form>
     <label for='email'> Enter your email: </label>
-    <input action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" type='email' name='name' id='email' method='post'>
+    <input action= "<?php
+echo htmlspecialchars($_SERVER["PHP_SELF"]);
+?>" type='email' name='name' id='email' method='post'>
     <input type='submit' value='Submit'>
   </form>
   </body>
@@ -15,11 +17,11 @@
 <?php
 $form = INPUT_POST;
 if (filter_has_var($form, "hoi")) {
-  if (filter_var($form, FILTER_VALIDATE_EMAIL)) {
-      echo("$form is a valid email address");
-  } else {
-      echo("$form is not a valid email address");
-  }
+    if (filter_var($form, FILTER_VALIDATE_EMAIL)) {
+        echo ("$form is a valid email address");
+    } else {
+        echo ("$form is not a valid email address");
+    }
 }
 
- ?>
+?>
