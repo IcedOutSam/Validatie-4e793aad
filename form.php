f3977752cdb33da6500219a26e052cddfce0419b
Sam <2102926@talnet.nl>
2020-04-24 12:45:41 +0200

    Uitwerking van de exercise

--- form.php
+++ form.php
@@ -1,5 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form>
+    <label for='email'> Enter your email: </label>
+    <input action= "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" type='email' name='name' id='email' method='post'>
+    <input type='submit' value='Submit'>
+  </form>
+  </body>
+</html>
 <?php
-echo "<label for='email'> Enter your email: </label> ";
-echo "<input type='text' name='name' id='email'>";
-echo "<input type='button' value='Submit'> ";
+$form = INPUT_POST;
+if (filter_has_var($form, "hoi")) {
+  if (filter_var($form, FILTER_VALIDATE_EMAIL)) {
+      echo("$form is a valid email address");
+  } else {
+      echo("$form is not a valid email address");
+  }
+}
+
  ?>
